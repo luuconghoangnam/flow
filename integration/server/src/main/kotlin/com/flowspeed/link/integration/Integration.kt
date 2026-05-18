1096639858170945aca95fffbdb045cd7faebe1a
luuconghoangnam <congll.a8k46gtb@gmail.com>
2026-05-18 11:31:02 +0700

    Add copyright notices: NOTICE file, LICENSE copyright, headers in 18 key source files

--- integration/server/src/main/kotlin/com/flowspeed/link/integration/Integration.kt
+++ integration/server/src/main/kotlin/com/flowspeed/link/integration/Integration.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2025 Luu Cong Hoang Nam
+ * Licensed under the Apache License, Version 2.0
+ * https://github.com/luuconghoangnam/flowspeed.link
+ */
 package com.flowspeed.link.integration
 
 import com.flowspeed.link.integration.http4k.MyHttp4KServer
